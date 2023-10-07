37b7818337da422298a59a305495d621352cb4ea
WillCQ97 <willian.cqueiroz@gmail.com>
2023-10-07 10:15:18 -0300

    aula 30 separando a página em componentes

--- pages/index.vue
+++ pages/index.vue
@@ -4,38 +4,29 @@
       <h1>Get the lastest tech news!</h1>
     </section>
     <section class="featured-posts">
-      <nuxt-link :to="'/posts/' + 1" class="post-preview">
-        <article>
-          <div
-            class="post-thumbnail"
-            style="
-              background-image: url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftraverseanywhere.files.wordpress.com%2F2020%2F07%2Ftechnology-watch.jpg&f=1&nofb=1&ipt=f8c750e3c3229431c54ba734a6504da44fdc4f5c3695062691c4d1a848af948c&ipo=images');
-            "
-          ></div>
-          <div class="post-content">
-            <h1>Post Title 1</h1>
-            <p>Preview Text 1</p>
-          </div>
-        </article>
-      </nuxt-link>
-      <nuxt-link :to="'/posts/' + 2" class="post-preview">
-        <article>
-          <div
-            class="post-thumbnail"
-            style="
-              background-image: url('https://www.aesc.org/sites/default/files/uploads/images/tech-og.jpg');
-            "
-          ></div>
-          <div class="post-content">
-            <h1>Post Title 2</h1>
-            <p>Preview Text 2</p>
-          </div>
-        </article>
-      </nuxt-link>
+      <PostPreview
+        id="1"
+        thumbnail="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftraverseanywhere.files.wordpress.com%2F2020%2F07%2Ftechnology-watch.jpg&f=1&nofb=1&ipt=f8c750e3c3229431c54ba734a6504da44fdc4f5c3695062691c4d1a848af948c&ipo=images"
+        title="Hello there"
+        preview-text="This is my first post!"
+      />
+      <PostPreview
+        id="2"
+        thumbnail="https://www.aesc.org/sites/default/files/uploads/images/tech-og.jpg"
+        title="Some Ai Stuff"
+        preview-text="Becuse its on mainstream!"
+      />
     </section>
   </div>
 </template>
-
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+export default {
+  components: {
+    PostPreview,
+  },
+}
+</script>
 <style scoped>
 .intro {
   height: 300px;
@@ -74,41 +65,5 @@
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-}
-
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
 }
 </style>
