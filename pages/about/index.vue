4ce88d8a1b417bf661ebdd0cae62611aca0e713a
WillCQ97 <willian.cqueiroz@gmail.com>
2023-10-07 00:14:18 -0300

    aula 27 página sobre e páginas de posts

---
+++ pages/about/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about-page">
+    <h1>This is my absolutely fantastic "About Me" Page!</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+      beatae, eaque est sapiente quasi illum recusandae obcaecati dicta ullam
+      assumenda odit labore quae soluta ducimus doloremque repellat fugiat
+      expedita architecto.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+      molestiae iure facilis fugit ullam magnam? Dolorem ea facere nostrum
+      beatae distinctio veniam, non dignissimos magnam eius nisi quaerat nemo
+      dolores!
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis pariatur
+      eaque laudantium, nulla nihil velit cupiditate illum minus animi iste
+      dolores quis molestiae fugiat eos provident, placeat voluptatum
+      reprehenderit aliquid?
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
