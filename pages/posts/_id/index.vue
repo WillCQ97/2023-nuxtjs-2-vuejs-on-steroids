6a7fef16da2297ce4ffa32e6798dcb7bc7c579ec
WillCQ97 <willian.cqueiroz@gmail.com>
2023-10-08 22:51:23 -0300

    aula 46 apenas mostrando a criação de um erro no callback

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -22,18 +22,8 @@
 <script>
 export default {
   asyncData(context, callback) {
-    callback(null, {
-      loadedPost: {
-        id: '0696f357-001a-4cd9-b9c5-7a356b9be204',
-        title: 'Hello there (ID: ' + context.route.params.id + ')',
-        previewText: 'This is my first post!',
-        author: 'Maximilian Schwarzmüller',
-        updatedDate: new Date(),
-        content:
-          'Some dumy text which is definitely not the preview text though.',
-        thumbnail:
-          'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftraverseanywhere.files.wordpress.com%2F2020%2F07%2Ftechnology-watch.jpg&f=1&nofb=1&ipt=f8c750e3c3229431c54ba734a6504da44fdc4f5c3695062691c4d1a848af948c&ipo=images',
-      },
+    return new Promise().catch(e => {
+      context.error(new Error("error"));
     })
   },
 }
